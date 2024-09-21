acda24dd739cfbf5df346cdbac4dc3487eae5b04
Rifat Arefin <arefinrifat786@gmail.com>
2024-09-21 11:22:23 +0600

    Remove mobile nav content

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -29,9 +29,9 @@
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <x-nav-link href="/" :active="request()->is('/')"> Home </x-nav-link>
-                                <x-nav-link href="/about" :active="request()->is('about')"> About </x-nav-link>
-                                <x-nav-link href="/contact" :active="request()->is('contact')"> Contact </x-nav-link>
+                                <x-nav-link href="/" :active="request()->is('/')" type="a"> Home </x-nav-link>
+                                <x-nav-link href="/about" :active="request()->is('about')" type="a"> About </x-nav-link>
+                                <x-nav-link href="/contact" :active="request()->is('contact')" type="a"> Contact </x-nav-link>
                             </div>
                         </div>
                     </div>
@@ -63,26 +63,6 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="-mr-2 flex md:hidden">
-                        <!-- Mobile menu button -->
-                        <button type="button"
-                            class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                            aria-controls="mobile-menu" aria-expanded="false">
-                            <span class="absolute -inset-0.5"></span>
-                            <span class="sr-only">Open main menu</span>
-                            <!-- Menu open: "hidden", Menu closed: "block" -->
-                            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                            </svg>
-                            <!-- Menu open: "block", Menu closed: "hidden" -->
-                            <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
                     </div>
                 </div>
             </div>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,13 +1,20 @@
-@props(['active' => false])
+@props(['active' => false, 'type' => "a"])
 
 @php
     if ($active) {
         $active_class = 'bg-gray-900 text-white';
     } else {
         $active_class = 'text-gray-300 hover:bg-gray-700 hover:text-white';
-    }
-        
+    }  
 @endphp
 
-<a {{$attributes}} class="{{$active_class}} rounded-md px-3 py-2 text-sm font-medium text-white" 
+
+@if ($type === "a")
+    <a {{$attributes}} class="{{$active_class}} rounded-md px-3 py-2 text-sm font-medium text-white" 
     aria-current="{{ $active ? 'page' : 'false' }}"> {{$slot}} </a>
+@elseif ($type === "button")
+    <button {{$attributes}} class="{{$active_class}} rounded-md px-3 py-2 text-sm font-medium text-white" 
+    aria-current="{{ $active ? 'page' : 'false' }}"> {{$slot}} </button>
+@else
+    <strong> {{$slot}} </strong>
+@endif
